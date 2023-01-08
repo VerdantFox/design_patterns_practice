a67cab0d53e57fb139ee7c134257cbcd143a9332
Teddy Williams <theodore.f.williams@gmail.com>
2023-01-08 12:39:22 -0700

    Add chapter 1 examples.

---
+++ chapter_1/action_game.py
@@ -0,0 +1,121 @@
+"""Action adventure puzzle
+
+Your task:
+
+Arrange the classes.
+
+Identify one abstract class, one interface, and eight classes.
+
+Draw arrows between classes.
+
+a. Draw this kind of arrow for inheritance (“extends”). Images
+
+b. Draw this kind of arrow for interface (“implements”). Images
+
+c. Draw this kind of arrow for HAS-A. Images
+
+Put the method setWeapon() into the right class.
+
+"""
+
+
+class WeaponBehavior:
+    """Base Weapon behavior class."""
+
+    def use_weapon(self):
+        """Use a weapon."""
+        raise NotImplementedError("Must implement for each weapon class.")
+
+
+class KnifeBehavior(WeaponBehavior):
+    """KnifeBehavior is a WeaponBehavior"""
+
+    def use_weapon(self):
+        """Use knife weapon."""
+        print("Stabbing!")
+
+
+class BowAndArrowBehavior(WeaponBehavior):
+    """BowAndArrowBehavior is a WeaponBehavior."""
+
+    def use_weapon(self):
+        """Use bow and arrow weapon."""
+        print("Shooting!")
+
+
+class AxeBehavior(WeaponBehavior):
+    """AxeBehavior is a WeaponBehavior."""
+
+    def use_weapon(self):
+        """Use an axe."""
+        print("Chopping!")
+
+
+class SwordBehavior(WeaponBehavior):
+    """SwordBehavior is a WeaponBehavior."""
+
+    def use_weapon(self):
+        """Use sword."""
+        print("Cutting!")
+
+
+class Character:
+    """Base character class. Has a weapon."""
+
+    def __init__(self) -> None:
+        self.weapon_behavior = None
+
+    def set_weapon(self, weapon_behavior: WeaponBehavior):
+        """Set the weapon behavior to something new."""
+        self.weapon_behavior = weapon_behavior
+
+    def fight(self) -> None:
+        """Fight with chosen weapon."""
+        self.weapon_behavior.use_weapon()
+
+
+class Queen(Character):
+    """Queen is a Character. Has a weapon."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.weapon_behavior = KnifeBehavior()
+
+
+class King(Character):
+    """King is a Character. Has a weapon."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.weapon_behavior = BowAndArrowBehavior()
+
+
+class Troll(Character):
+    """Troll is a Character. Has a weapon."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.weapon_behavior = AxeBehavior()
+
+
+class Knight(Character):
+    """Knight is a Character. Has a weapon."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.weapon_behavior = SwordBehavior()
+
+
+class ActionGame():
+
+    def main(self):
+        king = King()
+        troll = Troll()
+        king.fight()
+        troll.fight()
+        king.set_weapon(SwordBehavior())
+        king.fight()
+
+
+if __name__ == "__main__":
+    ActionGame().main()
---
+++ chapter_1/duck.py
@@ -0,0 +1,161 @@
+"""Implementation of the simulated duck."""
+
+
+class FlyBehavior:
+    """Fly behavior interface."""
+
+    def fly(self):
+        """Fly that duck."""
+        raise NotImplementedError(
+            "Implement the fly behavior of a specific duck flight."
+        )
+
+
+class FlyWithWings(FlyBehavior):
+    """Fly with wings interface."""
+
+    def fly(self):
+        "Fly for real."
+        print("I'm flying!")
+
+
+class FlyNoWay(FlyBehavior):
+    """Can't fly interface."""
+
+    def fly(self):
+        """Don't fly."""
+        print("I can't fly...")
+
+
+class FlyRocketPowered(FlyBehavior):
+    """Fly with rockets."""
+
+    def fly(self):
+        """Fly with rockets."""
+        print("I'm flying with a rocket!")
+
+
+class QuackBehavior:
+    """Quack behavior interface."""
+
+    def quack(self):
+        """Quack behavior interface."""
+        raise NotImplementedError(
+            "Implement the quack behavior for a specific duck quack"
+        )
+
+
+class Quack(QuackBehavior):
+    """Quack behavior interface for a quacking duck."""
+
+    def quack(self):
+        """Quack duck, quack!"""
+        print("Quack!")
+
+
+class MuteQuack(QuackBehavior):
+    """Quack behavior for ducks that can't quack."""
+
+    def quack(self):
+        """Don't quack!"""
+        print("<<silence>>")
+
+
+class Squeak(QuackBehavior):
+    """Squeak behavior for ducks that squeak."""
+
+    def quack(self):
+        """Squeak!"""
+        print("Squeak!")
+
+
+class Duck:
+    """SimDuck"""
+
+    def __init__(self):
+        """Init the duck class with the encapsulated methods.
+
+        In the future, these attributes could be set at runtime with this method,
+        but for now we'll set them like so.
+        """
+        self.fly_behavior = None
+        self.quack_behavior = None
+
+    def display(self):
+        """Display duck. This is different for every duck."""
+        raise NotImplementedError("Please override the display method for your duck.")
+
+    def perform_fly(self):
+        """Perform a duck fly."""
+        self.fly_behavior.fly()
+
+    def perform_quack(self):
+        """Perform a duck quack."""
+        self.quack_behavior.quack()
+
+    def set_quack_behavior(self, quack_behavior):
+        """Set the quack behavior."""
+        self.quack_behavior = quack_behavior
+
+    def set_fly_behavior(self, fly_behavior):
+        "Set the fly behavior."
+        self.fly_behavior = fly_behavior
+
+
+class MallardDuck(Duck):
+    """A mallard duck."""
+
+    def __init__(self):
+        """Reimplement the init from duck to provide defaults.
+
+        These could be provided at runtime instead."""
+        super().__init__()
+        self.fly_behavior = FlyWithWings()
+        self.quack_behavior = Quack()
+
+    def display(self):
+        """Display the mallard duck."""
+        print("I'm a real mallard duck!")
+
+
+class ModelDuck(Duck):
+    def __init__(self):
+        super().__init__()
+        self.fly_behavior = FlyNoWay()
+        self.quack_behavior = Quack()
+
+    def display(self):
+        """Display the model duck"""
+        print("I'm a model duck.")
+
+
+class DuckCall:
+    """Duck call that is not a duck but can use a quack"""
+
+    def __init__(self) -> None:
+        """Still use composition in case there are different types of quack."""
+        self.quack_behavior = Quack()
+
+    def quack(self):
+        """Quack calls the quack behavior."""
+        self.quack_behavior.quack()
+
+
+class MiniDuckSimulator:
+    """Duck simulator game."""
+
+    def main(self):
+        """Run the simulator for the mallard duck."""
+        mallard = MallardDuck()
+        mallard.display()  # I added this one in...
+        mallard.perform_quack()
+        mallard.perform_fly()
+
+        model = ModelDuck()
+        model.perform_fly()
+        model.set_fly_behavior(FlyRocketPowered())
+        model.perform_fly()
+
+
+if __name__ == "__main__":
+    MiniDuckSimulator().main()
